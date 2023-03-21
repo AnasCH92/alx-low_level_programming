8b8135caf01ff9fc9fabaee40871c498fbcbefcd
Anas Chakir <anas_chakir@outlook.com>
2023-03-21 16:46:49 -0700

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int count =0;
+	char letter;
+	while(count++ <=9)
+}
+for(letter ='a'; letter <= 'z' letter++)
+_putchar (leter);
+_putchar('\n');
+}
+}

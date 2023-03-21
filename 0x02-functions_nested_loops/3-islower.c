87512c698487fa887532bd60307e656387f13fab
Anas Chakir <anas_chakir@outlook.com>
2023-03-21 16:54:19 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * return : 0
+ */
+
+int main (void);
+
+int a = _islower;
+{
+	if (_islower >= 'a' && _islower <= 'z')
+		return (1);
+	esle
+		return(0);
+}

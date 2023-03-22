6e9dfa21b65a959c3edbc4b93343bcf989eea846
Anas Chakir <anas_chakir@outlook.com>
2023-03-21 17:19:28 -0700

    sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printSign(int num) {
+    if (num > 0) {
+        printf("%d is positive.\n", num);
+    } else if (num < 0) {
+        printf("%d is negative.\n", num);
+    } else {
+        printf("%d is zero.\n", num);
+    }
+}
+

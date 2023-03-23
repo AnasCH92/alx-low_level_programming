725614eda8fe447e0e2acff83c84d41b8696d76b
Anas Chakir <anas_chakir@outlook.com>
2023-03-23 11:08:00 -0700

    SD

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#ifndef main_h
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b,int c);
+int convert_days (int month, int day, int year,);
+
 
 #endif

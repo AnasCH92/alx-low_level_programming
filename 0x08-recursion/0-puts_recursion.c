fc09fa059e47ee2058bc8e0733b5d5386cbe9b75
Anas Chakir <anas_chakir@outlook.com>
2023-04-05 18:51:36 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ *	 _puts_recursion("Puts with recursion");
+ *	 return (0);
+ */
+
+void _puts_recursion(char *a)
+{
+	if(*(a) != '\0')
+	{
+		_putchar(*a);
+		_put_recursion(a+1);
+	}
+	if (*(a) == '\0')
+	{
+		_putchar('\n');
+	}
+}
